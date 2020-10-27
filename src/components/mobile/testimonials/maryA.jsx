43df952246db846d65b51e3b15af0c8d0b9bca82
Isaac Householder <isaachouseholder@gmail.com>
2020-10-27 15:47:51 -0700

    Edits on CAA Website - Line 10

--- src/components/mobile/testimonials/maryA.jsx
+++ src/components/mobile/testimonials/maryA.jsx
@@ -8,7 +8,7 @@ export default function MaryA({desktop}) {
       <div className={desktop ? "testimonialReviewDesktop" : "testimonialReview"}>
         To compare Liennette to others at her professional level is to compare
         an Olympic athlete to a collegiate player in sports. She develops a
-        vision of the goal. then develops and executes a successful gameplan.She
+        vision of the goal. then develops and executes a successful gameplan. She
         is able to achieve her goals in a manner that is quicker, more focused,
         and more insightful.
       </div>{" "}
